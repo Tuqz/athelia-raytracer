692afe35ec722f256adae3e8ce42deaeeae5363a
James <jamesrawson628@gmail.com>
2014-08-17 20:29:52 +0100

    Added lua test and lua interface

--- hello.lua
+++ hello.lua
@@ -4,4 +4,21 @@ a = athelia.Vector();
 a.x = 1;
 a.y = 2;
 a.z = 3;
-print(a.y)
+b = athelia.Vector();
+b.x = 3;
+b.y = 2;
+b.z = 1;
+
+c = a*2
+d = a+b
+e = a:dot(b)
+f = a:cross(b)
+g = a:size()
+h = a:magnitude()
+
+print(c.x)
+print(d.x)
+print(e)
+print(f.x)
+print(g)
+print(h)
